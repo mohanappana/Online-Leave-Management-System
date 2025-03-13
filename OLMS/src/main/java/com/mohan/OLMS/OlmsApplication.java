96cdcd836e0bf9cb8cb04173f62b29ce19150e07
mohan kumar <mohanappana2001@gmail.com>
2025-03-13 18:02:49 +0530

    update to use railway environmental variables

--- OLMS/src/main/java/com/mohan/OLMS/OlmsApplication.java
+++ OLMS/src/main/java/com/mohan/OLMS/OlmsApplication.java
@@ -1,20 +1,9 @@
 package com.mohan.OLMS;
-
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OlmsApplication {
-
-	static {
-		// Load the .env file
-		Dotenv dotenv = Dotenv.load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlmsApplication.class, args);
