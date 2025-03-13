6a23c9bd98c2a8b32f4ce0180d661de1a66344f2
mohan kumar <mohanappana2001@gmail.com>
2025-03-13 12:26:27 +0530

    add datasoure and modify some styles

--- OLMS/src/main/java/com/mohan/OLMS/OlmsApplication.java
+++ OLMS/src/main/java/com/mohan/OLMS/OlmsApplication.java
@@ -1,10 +1,19 @@
 package com.mohan.OLMS;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OlmsApplication {
+
+	static {
+		// Load the .env file
+		Dotenv dotenv = Dotenv.load();
+
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.out.println(dotenv.get("DB_PASSWORD"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlmsApplication.class, args);
