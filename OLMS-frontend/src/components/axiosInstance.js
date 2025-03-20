9769a1ba9e440422a09fc362431934fdfa854f3b
mohan kumar <140064153+mohanappana@users.noreply.github.com>
2025-03-20 09:12:08 +0530

    Update with render url axiosInstance.js

--- OLMS-frontend/src/components/axiosInstance.js
+++ OLMS-frontend/src/components/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://olms-backend-v1.onrender.com",
     headers: {
     "Content-Type": "application/json",
   },
